f2b461d8a068c465018d3d44bc96bcfa9066aeb2
VArtz <farrelnikoson@gmail.com>
2024-06-01 09:42:38 +0700

    Renew port with process.env.port

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./application/web.js"
 import { logger } from "./application/logging.js"
 
-web.listen(3000, () => {
+web.listen(process.env.PORT || 3000, () => {
     logger.info("Server started on port 3000")
 })
